33cfee054be1f089ad167073398e14f1f0e67074
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2024-01-11 13:14:34 -0500

    gfhgfdgfdg

--- create-task/main.js
+++ create-task/main.js
@@ -1,1 +1,30 @@
 /* emmetapp, animate on scroll */
+const sampleArr = {};
+const API = "https://opentdb.com/api.php?amount=10";
+function cardCreator(arr) {
+  arr.forEach((i) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = ``;
+    DOMSelectors.itemcontainer.appendChild(card);
+  });
+}
+async function createArr() {
+  try {
+    const response = await fetch(API);
+    const data = await response.json();
+    console.log(data.response_code);
+    const cluebaseAPI = data.results;
+    const cluebaseArr = cluebaseAPI.map((arr) => ({
+      question: arr.question,
+      answer: arr.correct_answer,
+      incResponse1: arr.incorrect_answers[1],
+      incResponse2: arr.incorrect_answers[2],
+      incResponse3: arr.incorrect_answers[3],
+    }));
+    console.log(cluebaseArr);
+  } catch (error) {
+    console.log("uhoh");
+  }
+}
+createArr();
